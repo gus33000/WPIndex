42fb770055aafeaf729fd292e704d5031678461e
Gustave Monce <gustave.monce@outlook.com>
2021-07-26 23:22:40 +0200

    Add project files.

---
+++ WPIndex/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPIndex
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string filelist = @"W:\cablist.csv";
+
+            Console.WriteLine("Reading all lines");
+            var files = File.ReadAllLines(filelist);
+
+            /*var newfilelist = files.Select(x =>
+            {
+                Console.Title = x;
+                using var strm = File.OpenRead(x.Split(",")[1]);
+                return $"{x.Split(",")[1]},{x.Split(",")[0]},{strm.GetCabinetType()}";
+            });
+
+            Console.WriteLine("Backing up data");
+            File.WriteAllLines(@"W:\cablist.type.csv", newfilelist);*/
+
+            await Parallel.ForEachAsync(files, (file, _) =>
+            {
+                Console.WriteLine(file);
+                using var strm = File.OpenRead(file.Split(",")[1]);
+                switch (strm.GetCabinetType())
+                {
+                    case XmlHelper.CabinetType.Broken:
+                        Console.WriteLine("BROKEN");
+                        break;
+                    case XmlHelper.CabinetType.Unknown:
+                        Console.WriteLine("UNKNOWN");
+                        break;
+                    case XmlHelper.CabinetType.CBS:
+                    case XmlHelper.CabinetType.CBSU:
+                        {
+                            using var xmlfile = new MemoryStream(Cabinet.CabinetExtractor.ExtractCabinetFile(strm, "update.mum"));
+                            var pkg = xmlfile.GetUpdateMum();
+                            try
+                            {
+                                Console.WriteLine("pkg.AssemblyIdentity.Version: " + pkg.AssemblyIdentity.Version);
+                            }
+                            catch { }
+                            try
+                            {
+                                Console.WriteLine("pkg.ManifestVersion: " + pkg.ManifestVersion);
+                            }
+                            catch { }
+                            try
+                            {
+                                Console.WriteLine("pkg.Package.Update.Component.AssemblyIdentity.Version: " + pkg.Package.Update.Component.AssemblyIdentity.Version);
+                            }
+                            catch { }
+                            break;
+                        }
+                    case XmlHelper.CabinetType.SPKG:
+                    case XmlHelper.CabinetType.SPKU:
+                        {
+                            try
+                            {
+                                using var xmlfile = new MemoryStream(Cabinet.CabinetExtractor.ExtractCabinetFile(strm, "man.dsm.xml"));
+                                var pkg = xmlfile.GetManDSMXml();
+                                try
+                                {
+                                    Console.WriteLine("pkg.Identity.Version: " + pkg.Identity.Version.Major + "." + pkg.Identity.Version.Minor + "." + pkg.Identity.Version.QFE + "." + pkg.Identity.Version.Build);
+                                }
+                                catch { }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("CRASH: " + file);
+                                throw;
+                            }
+                            break;
+                        }
+
+                }
+
+                return ValueTask.CompletedTask;
+            });
+
+            Console.WriteLine("The end");
+        }
+    }
+}
---
+++ WPIndex/XmlHelper.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace WPIndex
+{
+    public static class XmlHelper
+    {
+        public static XmlMum.Assembly GetUpdateMum(this Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(XmlMum.Assembly));
+            XmlMum.Assembly package = (XmlMum.Assembly)serializer.Deserialize(stream);
+            return package;
+        }
+
+        public static XmlDsmDiff.DiffPackage GetDManDDSMXml(this Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(XmlDsmDiff.DiffPackage));
+            XmlDsmDiff.DiffPackage package = (XmlDsmDiff.DiffPackage)serializer.Deserialize(stream);
+            return package;
+        }
+
+        public static XmlDsm.Package GetManDSMXml(this Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(XmlDsm.Package));
+            XmlDsm.Package package = (XmlDsm.Package)serializer.Deserialize(stream);
+            return package;
+        }
+
+        public static BuildInfo.Buildinformation GetBuildInfoXml(this Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(BuildInfo.Buildinformation));
+            BuildInfo.Buildinformation package = (BuildInfo.Buildinformation)serializer.Deserialize(stream);
+            return package;
+        }
+
+        public static string GetFileLocationInCab(this Stream cab, string filename)
+        {
+            var files = Cabinet.CabinetExtractor.EnumCabinetFiles(cab);
+            if (files.Any(x => x.EndsWith("update.mum", System.StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var updatemum = Cabinet.CabinetExtractor.ExtractCabinetFile(cab, files.First(x => x.EndsWith("update.mum", System.StringComparison.InvariantCultureIgnoreCase)));
+
+                using var stream = new MemoryStream(updatemum);
+                var package = stream.GetUpdateMum();
+
+                var entry = package.Package.CustomInformation.File.First(x => x.Name.EndsWith(filename, System.StringComparison.InvariantCultureIgnoreCase));
+                return entry.Cabpath;
+            }
+            else if (files.Any(x => x.EndsWith("man.dsm.xml", System.StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var mandsmxml = Cabinet.CabinetExtractor.ExtractCabinetFile(cab, files.First(x => x.EndsWith("man.dsm.xml", System.StringComparison.InvariantCultureIgnoreCase)));
+
+                using var stream = new MemoryStream(mandsmxml);
+                var package = stream.GetManDSMXml();
+
+                var entry = package.Files.FileEntry.First(x => x.DevicePath.EndsWith(filename, System.StringComparison.InvariantCultureIgnoreCase));
+                return entry.CabPath;
+            }
+            else
+            {
+                throw new InvalidDataException();
+            }
+        }
+
+        public enum CabinetType
+        {
+            CBS,
+            CBSU,
+            SPKG,
+            SPKU,
+            Unknown,
+            Broken
+        }
+
+        public static CabinetType GetCabinetType(this Stream cab)
+        {
+            try
+            {
+                var files = Cabinet.CabinetExtractor.EnumCabinetFiles(cab);
+                if (files.Any(x => x.EndsWith("_manifest_.cix.xml", System.StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return CabinetType.CBSU;
+                }
+                else if (files.Any(x => x.EndsWith("update.mum", System.StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return CabinetType.CBS;
+                }
+                else if (files.Any(x => x.EndsWith("dman.ddsm.xml", System.StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return CabinetType.SPKU;
+                }
+                else if (files.Any(x => x.EndsWith("man.dsm.xml", System.StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    return CabinetType.SPKG;
+                }
+                else
+                {
+                    return CabinetType.Unknown;
+                }
+            }
+            catch
+            {
+                return CabinetType.Broken;
+            }
+        }
+    }
+}
